84957aaafa24c8dfead5f442f6e2962f49aa725b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-14 17:16:38 +0200

    Snake grows when eats apple

--- code/snake/src/main.rs
+++ code/snake/src/main.rs
@@ -1,20 +1,14 @@
-#[derive(Debug, PartialEq, Clone)]
-struct Segment(u32, u32);
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct Point(u32, u32);
 
-impl Segment {
-    fn x(&self) -> u32 {
+impl Point {
+    pub fn x(&self) -> u32 {
         self.0
-    } 
+    }
 
-    fn y(&self) -> u32 {
+    pub fn y(&self) -> u32 {
         self.1
     }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Snake {
-    pub segments: Vec<Segment>,
-    is_alive_: bool
 }
 
 pub enum Direction {
@@ -24,71 +18,130 @@ pub enum Direction {
     Down,
 }
 
-impl Snake {
-    pub fn new(segments: Vec<(u32, u32)>) -> Snake {
-        let mut snake = Snake{segments: Vec::new(), is_alive_: false };
+#[derive(Debug)]
+pub struct Snake<'a> {
+    pub segments: Vec<Point>,
+    _board: Option<&'a mut Board>,
+    is_alive_: bool,
+}
+
+impl<'a> Snake<'a> {
+    pub fn new(segments: Vec<(u32, u32)>) -> Snake<'a> {
+        let mut snake = Snake {
+            segments: Vec::new(),
+            _board: None,
+            is_alive_: true,
+        };
         for s in segments {
-            snake.segments.push(Segment(s.0, s.1));
+            snake.segments.push(Point(s.0, s.1));
         }
         snake
     }
 
-    pub fn head(&self) -> &Segment {
+    pub fn head(&self) -> &Point {
         &self.segments[0]
     }
 
-    pub fn tail(&self) -> &Segment {
-        &self.segments[self.segments.len() - 1]
-    }
-
     pub fn crawl(&mut self, direction: Direction) {
-        let Segment(x, y) = *self.head();
+        let Point(x, y) = *self.head();
 
         let new_head = match direction {
-            Direction::Left => Segment(x - 1, y),
-            Direction::Right => Segment(x + 1, y),
-            Direction::Up => Segment(x, y - 1),
-            Direction::Down => Segment(x, y + 1),
+            Direction::Left => Point(x - 1, y),
+            Direction::Right => Point(x + 1, y),
+            Direction::Up => Point(x, y - 1),
+            Direction::Down => Point(x, y + 1),
         };
 
         self.segments.insert(0, new_head.clone());
-        self.segments.pop();
+       
+        if let Some(ref mut board ) = self._board {
+            if new_head.x() == board.width()
+            || new_head.y() == board.height()
+            || new_head.x() == 0
+            || new_head.y() == 0
+            {
+                self.is_alive_ = false;
+            }
 
-        if new_head.x() == 0 || new_head.y() == 0 {
-            self.is_alive_ = false;
+            if board.try_to_eat_apple(new_head) {
+                return;
+            }
         }
+
+        self.segments.pop();
     }
 
     pub fn is_alive(&self) -> bool {
         self.is_alive_
     }
+
+    pub fn set_board(&mut self, board: &'a mut Board) {
+        self._board = Some(board);
+    }
+}
+
+impl PartialEq for Snake<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.segments == other.segments
+    }
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Board(u32, u32);
+pub struct Board {
+    _width: u32,
+    _height: u32,
+    _apples: Vec<Point>,
+}
 
 impl Board {
+    pub fn new(width: u32, height: u32) -> Board {
+        Board {
+            _width: width,
+            _height: height,
+            _apples: Vec::new(),
+        }
+    }
+
     pub fn width(&self) -> u32 {
-        return self.0;
+        return self._width;
     }
 
     pub fn height(&self) -> u32 {
-        return self.1;
+        return self._height;
+    }
+    pub fn add_apple(&mut self, apple: Point) {
+        self._apples.push(apple);
+    }
+
+    pub fn apples(&self) -> &Vec<Point> {
+        &self._apples
+    }
+
+    pub fn try_to_eat_apple(&mut self, point: Point) -> bool {        
+        if let Some(index) = self._apples.iter().position(|&p| p == point) {
+            self._apples.remove(index);
+            return true;
+        }
+        false
     }
 }
 
 #[cfg(test)]
 mod snake_tests {
-    use rstest::rstest;
+    use rstest::{fixture, rstest};
 
-    use crate::{Direction, Snake, Board, Segment};
+    use crate::{Board, Direction, Point, Snake};
 
     #[test]
     fn snake_constructed_with_segments() {
         let snake = Snake::new(vec![(0, 0), (1, 0), (2, 0)]);
 
-        assert_eq!(snake.head(), &Segment(0, 0));
-        assert_eq!(snake.tail(), &Segment(2, 0));
+        assert_eq!(snake.head(), &Point(0, 0));
+    }
+
+    #[fixture]
+    fn board() -> Board {
+        Board::new(20, 10)
     }
 
     #[rstest]
@@ -106,16 +159,36 @@ mod snake_tests {
     }
 
     #[rstest]
-    #[case(Snake::new(vec![(10, 1)]), Direction::Up)]
-    #[case(Snake::new(vec![(10, 9)]), Direction::Down)]
-    #[case(Snake::new(vec![(1, 5)]), Direction::Left)]
-    #[case(Snake::new(vec![(9, 5)]), Direction::Right)]
-    fn snake_dies_when_hits_the_wall(#[case] mut snake: Snake, #[case] direction: Direction) {
-        let board = Board(20, 10);
+    #[case(vec![(10, 1)], Direction::Up)]
+    #[case(vec![(10, 9)], Direction::Down)]
+    #[case(vec![(1, 5)], Direction::Left)]
+    #[case(vec![(19, 5)], Direction::Right)]
+    fn snake_dies_when_hits_the_wall(
+        mut board: Board,
+        #[case] mut segments: Vec<(u32, u32)>,
+        #[case] direction: Direction,
+    ) {
+        let mut snake = Snake::new(segments);
+        snake.set_board(&mut board);
+
+        assert!(snake.is_alive());
 
         snake.crawl(direction);
 
         assert!(!snake.is_alive());
+    }
+
+    #[rstest]
+    fn when_snake_eats_apple_it_grows(mut board: Board) {
+        board.add_apple(Point(5, 4));
+
+        let mut snake = Snake::new(vec![(5, 5), (5, 6), (5, 7)]);
+        snake.set_board(&mut board);
+
+        snake.crawl(Direction::Up);
+        
+        assert_eq!(snake, Snake::new(vec![(5, 4), (5, 5), (5, 6), (5, 7)]));
+        assert_eq!(board.apples().len(), 0);
     }
 }
 
@@ -125,7 +198,7 @@ mod board_tests {
 
     #[test]
     fn board_has_width_and_height() {
-        let board = Board(10, 20);
+        let board = Board::new(10, 20);
 
         assert_eq!(board.width(), 10);
         assert_eq!(board.height(), 20);
