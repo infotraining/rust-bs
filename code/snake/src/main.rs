fa96db2adc04388d82dc03086403a9ffc1051a83
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-15 12:16:11 +0200

    SnakeGame with bracket-lib

--- code/snake/src/main.rs
+++ code/snake/src/main.rs
@@ -1,7 +1,17 @@
+use bracket_lib::prelude::{main_loop, BError, BTermBuilder};
+
 mod snake;
 mod board;
 mod snake_game;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> BError {
+    use snake_game::SnakeGame;
+
+    let context = BTermBuilder::simple80x50()
+        .with_title("Snake game")
+        .build()?;
+
+    let mut game = SnakeGame::new();
+
+    main_loop(context, game)
 }
--- code/snake/src/snake_game.rs
+++ code/snake/src/snake_game.rs
@@ -1,170 +1,161 @@
-use mockall::*;
-use mockall::predicate::*;
+use bracket_lib::prelude::*;
 
-use crate::board::Board;
-use crate::snake::{Point, Snake, Direction};
+use crate::{
+    board::Board,
+    snake::{Direction, Snake, Point},
+};
 
-pub struct SnakeGame<'a> {
+enum GameMode {
+    Menu,
+    Playing,
+    GameOver,
+}
+
+pub struct SnakeGame {
+    mode: GameMode,
     board: Board,
     snake: Snake,
-    terminal: &'a dyn Terminal
+    current_direction: Direction,
 }
 
-impl<'a> SnakeGame<'a> {
-    // pub fn new(width: u32, height: u32) -> SnakeGame {    
-    //     let game = SnakeGame {
-    //         board : Board::new(width, height),
-    //         snake : Snake::new(vec![(width / 2, height / 2), (width / 2, height / 2 + 1)])
-    //     };
-
-    //     game
-    // }
-
-    pub fn snake(&self) -> &Snake {
-        &self.snake
-    }
-
-    pub fn run(&self) {
-        self.terminal.render_board(&self.board);
-        self.terminal.render_apples(self.board.apples());
-    }
-}
-
-struct SnakeGameBuilder<'a> {
-    board_dimensions: (u32, u32),
-    apples: Vec<Point>,
-    terminal: Option<&'a dyn Terminal>
-}
-
-impl<'a> SnakeGameBuilder<'a> {
-    pub fn new() -> SnakeGameBuilder<'a> {
-        SnakeGameBuilder {
-            board_dimensions: (20, 10),
-            apples: Vec::new(),
-            terminal: None
-        }
-    }
-
-    pub fn with_board(mut self, width: u32, height: u32) -> Self {
-        self.board_dimensions = (width, height);
-        self
-    }
-
-    pub fn with_apples(mut self, apples: Vec<Point>) -> Self {
-        self.apples = apples;
-        self
-    }
-
-    pub fn with_terminal(mut self, terminal: &'a (dyn Terminal + 'a)) -> Self {
-        self.terminal = Some(terminal);
-        self
-    }
-
-    pub fn build(self) -> SnakeGame<'a> {
-        let mut board = Board::new(self.board_dimensions.0, self.board_dimensions.1);
-        for apple in self.apples {
-            board.add_apple(apple);
-        }
-
+impl SnakeGame {
+    pub fn new() -> Self {
         SnakeGame {
-            board: board,
-            snake: Snake::new(vec![(self.board_dimensions.0 / 2, self.board_dimensions.1 / 2), (self.board_dimensions.0 / 2, self.board_dimensions.1 / 2 + 1)]),
-            terminal: self.terminal.unwrap()
+            mode: GameMode::Menu,
+            board: Board::new(80, 50),
+            snake: Snake::new(vec![(40, 25), (41, 25), (42, 25)]),
+            current_direction: Direction::Right,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "~~ S N A K E ~~");
+        ctx.print_centered(8, "Press [P] to play");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                _ => {}
+            }
+        }
+    }
+
+    fn drop_apples(&mut self, count: u32) {
+        let mut rng = RandomNumberGenerator::new();
+
+        for _ in 0..count {
+            let x = rng.range(1, self.board.width() - 1);
+            let y = rng.range(1, self.board.height() - 1);
+
+            self.board.add_apple(Point(x, y));
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+
+        self.board = Board::new(80, 50);
+        self.snake = Snake::new(vec![(40, 25), (41, 25), (42, 25)]);
+        self.current_direction = Direction::Right;
+        self.drop_apples(10);
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Snake is dead !!!");
+        ctx.print_centered(8, "[P] Play again");
+        ctx.print_centered(9, "[Q] Quit game");
+
+        self.mode = GameMode::GameOver;
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::Playing;
+        ctx.cls();
+
+        self.render_board(ctx);
+        self.render_snake(ctx);
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::Escape => self.mode = GameMode::GameOver,
+                VirtualKeyCode::Up => self.current_direction = Direction::Up,
+                VirtualKeyCode::Down => self.current_direction = Direction::Down,
+                VirtualKeyCode::Left => self.current_direction = Direction::Left,
+                VirtualKeyCode::Right => self.current_direction = Direction::Right,
+                _ => {}
+            }
+        }
+
+        self.snake.move_to(self.current_direction, &mut self.board);
+
+        if !self.snake.is_alive() {
+            self.dead(ctx);
+        }
+    }
+
+    fn render_board(&self, ctx: &mut BTerm) {
+        for x in 0..self.board.width() {
+            ctx.set(x, 0, RGB::named(WHITE), RGB::named(BLACK), to_cp437('-'));
+            ctx.set(
+                x,
+                self.board.height() - 1,
+                RGB::named(WHITE),
+                RGB::named(BLACK),
+                to_cp437('-'),
+            );
+        }
+
+        for y in 0..self.board.height() {
+            ctx.set(0, y, RGB::named(WHITE), RGB::named(BLACK), to_cp437('|'));
+            ctx.set(
+                self.board.width() - 1,
+                y,
+                RGB::named(WHITE),
+                RGB::named(BLACK),
+                to_cp437('|'),
+            );
+        }
+
+        for apple in self.board.apples() {
+            ctx.set(
+                apple.x(),
+                apple.y(),
+                RGB::named(RED),
+                RGB::named(BLACK),
+                to_cp437('@'),
+            );
+        }
+    }
+
+    fn render_snake(&self, ctx: &mut BTerm) {
+        for segment in &self.snake.segments {
+            ctx.set(
+                segment.x(),
+                segment.y(),
+                RGB::named(GREEN),
+                RGB::named(BLACK),
+                to_cp437('#'),
+            );
         }
     }
 }
 
-
-#[automock]
-trait Terminal {
-    fn render_board(&self, board: &Board);
-    fn render_apples(&self, apples: &[Point]);
-}
-
-#[cfg(test)]
-mod snake_game_tests {
-    use mockall::predicate;
-    use crate::snake_game::{MockTerminal, SnakeGameBuilder};
-
-    use crate::snake::{Direction, Point, Snake};
-    
-
-    #[test]
-    fn when_game_starts_snake_is_in_the_middle_of_the_board()
-    {
-        let mock_terminal = MockTerminal::new();
-        let (width, height) = (20, 10);
-
-        let game = SnakeGameBuilder::new()
-            .with_board(width, height)
-            .with_terminal(&mock_terminal)
-            .build();
-
-        assert_eq!(game.snake(), &Snake::new(vec![(10, 5), (10, 6)]));
-
-    }
-
-    #[test]
-    fn when_game_starts_snake_is_alive()
-    {
-        let mock_terminal = MockTerminal::new();
-        let (width, height) = (20, 10);
-
-        let game = SnakeGameBuilder::new()
-            .with_board(width, height)
-            .with_terminal(&mock_terminal)
-            .build();
-
-        assert_eq!(game.snake().is_alive(), true);
-
-    }
-
-    #[test]
-    fn when_game_starts_snake_is_directed_up()
-    {
-        let mock_terminal = MockTerminal::new();
-        let (width, height) = (20, 10);
-
-        let game = SnakeGameBuilder::new()
-            .with_board(width, height)
-            .with_terminal(&mock_terminal)
-            .build();
-
-        assert_eq!(game.snake().direction(), Direction::Up);
-
-    }
-
-    #[test]
-    fn game_loop_renders_board() {
-        let mut mock_terminal = MockTerminal::new();
-        mock_terminal.expect_render_board().times(1).return_const(());
-        mock_terminal.expect_render_apples().times(1).return_const(());
-
-        let (width, height) = (20, 10);
-        let game = SnakeGameBuilder::new()
-            .with_board(width, height)
-            .with_terminal(&mut mock_terminal)
-            .build();
-
-        game.run();
-    }
-
-    #[test]
-    fn game_loop_renders_apples() {
-        let mut mock_terminal = MockTerminal::new();
-        mock_terminal.expect_render_board().times(1).return_const(());
-        mock_terminal.expect_render_apples()
-            .with(predicate::eq(vec![Point(3, 2), Point(6, 4)])).times(1).return_const(());
-
-        let (width, height) = (20, 10);
-        let game = SnakeGameBuilder::new()
-            .with_board(width, height)
-            .with_apples(vec![Point(3, 2), Point(6, 4)])
-            .with_terminal(&mut mock_terminal)
-            .build();
-
-        game.run();
+impl GameState for SnakeGame {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::GameOver => self.dead(ctx),
+        }
     }
 }
-
-
