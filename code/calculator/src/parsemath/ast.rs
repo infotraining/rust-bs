0815baf879600f11393f96781c7db9d5038dcf11
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-24 17:04:14 +0200

    Calculator - evaluating ast

--- code/calculator/src/parsemath/ast.rs
+++ code/calculator/src/parsemath/ast.rs
@@ -8,3 +8,56 @@ pub enum Expression {
     Negate(Box<Expression>),
     Grouping(Box<Expression>),
 }
+
+impl Expression {
+    fn eval(&self) -> f64 {
+        match self {
+            Expression::Number(n) => *n,
+            Expression::Add(a, b) => a.eval() + b.eval(),
+            Expression::Subtract(a, b) => a.eval() - b.eval(),
+            Expression::Multiply(a, b) => a.eval() * b.eval(),
+            Expression::Divide(a, b) => a.eval() / b.eval(),
+            Expression::Negate(expr) => -expr.eval(),
+            Expression::Grouping(expr) => expr.eval(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use crate::parsemath::ast::Expression;
+
+    #[rstest]
+    #[case::ast_add_1_2(
+        Expression::Add(
+            Box::new(Expression::Number(1.0)),
+            Box::new(Expression::Number(2.0))
+        ), 3.0)]
+    #[case::ast_mul_minus_1_2(
+        Expression::Multiply(
+            Box::new(Expression::Negate(Box::new(Expression::Number(1.0)))),
+            Box::new(Expression::Number(2.0))
+        ), -2.0)]
+    #[case::ast_group_add_1_2_mul_group_sub_3_4(
+        Expression::Multiply(
+            Box::new(Expression::Grouping(
+                Box::new(Expression::Add(
+                    Box::new(Expression::Number(1.0)),
+                    Box::new(Expression::Number(2.0))
+                ))
+            )),
+            Box::new(Expression::Grouping(
+                Box::new(Expression::Subtract(
+                    Box::new(Expression::Number(3.0)),
+                    Box::new(Expression::Number(4.0))
+                ))
+            ))
+        ), -3.0)]
+    fn eval_simple_expression(#[case] ast: Expression, #[case] expected: f64)
+    {
+        let result = ast.eval();
+
+        assert_eq!(result, expected);
+    }
+}
