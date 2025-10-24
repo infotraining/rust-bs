d4a6e643a1d944f636bd6b56cc3e7966009dd489
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-24 14:29:40 +0200

    Calculator - parsing (+, -, *, /)

---
+++ code/calculator/src/parsemath/ast.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expression {
+    Number(f64),
+    Add(Box<Expression>, Box<Expression>),
+    Subtract(Box<Expression>, Box<Expression>),
+    Multiply(Box<Expression>, Box<Expression>),
+    Divide(Box<Expression>, Box<Expression>),
+}
---
+++ code/calculator/src/parsemath/parser.rs
@@ -0,0 +1,240 @@
+use crate::parsemath::ast::{Expression};
+use crate::parsemath::token::Token;
+use crate::parsemath::token::Token::Number;
+use crate::parsemath::tokenizer::{Tokenizer, TokenizingError};
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum ParserError {
+    UnableToParse,
+}
+
+pub struct Parser {
+    tokens: Vec<Token>,
+    current_token_index: usize
+}
+
+impl Parser {
+    pub fn new(expression: &str) -> Result<Self, ParserError> {
+        let mut tokenizer = Tokenizer::new(expression);
+        let tokens = tokenizer.collect::<Result<Vec<Token>, TokenizingError>>();
+
+        match tokens {
+            Ok(tokens) => Ok(Parser{tokens, current_token_index: 0}),
+            Err(e) => Err(ParserError::UnableToParse)
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Expression, ParserError> {
+        let expression = self.expression();
+
+        Ok(expression)
+    }
+
+    fn expression(&mut self) -> Expression {
+        let expression = self.term();
+
+        expression
+    }
+
+    fn term(&mut self) -> Expression {
+        let mut expression = self.factor();
+
+        loop {
+            match self.peek() {
+                Some(Token::Add) => {
+                    self.consume();
+                    let right = self.factor();
+                    expression = Expression::Add(Box::new(expression), Box::new(right));
+                },
+                Some(Token::Subtract) => {
+                    self.consume();
+                    let right = self.factor();
+                    expression = Expression::Subtract(Box::new(expression), Box::new(right));
+                }
+                _ => break
+            }
+        }
+
+        expression
+    }
+
+    fn factor(&mut self) -> Expression {
+        let mut expression = self.unary();
+
+        loop {
+            match self.peek() {
+                Some(Token::Multiply) => {
+                    self.consume();
+                    let right = self.unary();
+                    expression = Expression::Multiply(Box::new(expression), Box::new(right));
+                },
+                Some(Token::Divide) => {
+                    self.consume();
+                    let right = self.unary();
+                    expression = Expression::Divide(Box::new(expression), Box::new(right));
+                }
+                _ => break
+            }
+        }
+
+        expression
+    }
+
+    fn unary(&mut self) -> Expression {
+        return self.primary();
+    }
+
+    fn primary(&mut self) -> Expression {
+        let expression = match self.next() {
+            Some(Token::Number(n)) => { Expression::Number(n) },
+            _ => panic!()
+        };
+
+        //self.advance();
+
+        expression
+    }
+
+    // fn match_token(&mut self, expected: Token) -> bool {
+    //     if self.check(expected) {
+    //         self.advance();
+    //         return true;
+    //     }
+    //     false
+    // }
+
+    // fn check(&mut self, expected: Token) -> bool {
+    //     if self.is_at_end() {
+    //         return false;
+    //     }
+    //
+    //     return self.peek() == expected;
+    // }
+
+    fn consume(&mut self) {
+        if !self.is_at_end() {
+            self.current_token_index += 1;
+        }
+    }
+
+    // fn advance(&mut self) -> Token {
+    //     if !self.is_at_end() {
+    //         self.current_token_index += 1;
+    //     }
+    //
+    //     return self.previous();
+    // }
+
+    fn is_at_end(&self) -> bool {
+        self.current_token_index >= self.tokens.len()
+    }
+
+    fn peek(&self) -> Option<Token> {
+        if self.is_at_end() {
+            return None
+        }
+        Some(self.tokens[self.current_token_index].clone())
+    }
+
+    fn next(&mut self) -> Option<Token> {
+        let token = self.peek();
+        self.current_token_index += 1;
+        return token;
+    }
+
+    // fn previous(&self) -> Token {
+    //     self.tokens[self.current_token_index - 1].clone()
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+use rstest::*;
+    use crate::parsemath::ast::Expression;
+    use crate::parsemath::parser::Parser;
+
+    #[rstest]
+    fn parse_to_ast() {
+        let expression = "1";
+        let mut parser = Parser::new(expression).unwrap();
+
+        let ast = parser.parse().unwrap();
+        assert_eq!(ast, Expression::Number(1.0));
+    }
+
+    #[rstest]
+    #[case::expr_1_times_2("1 * 2", Expression::Multiply(Box::new(Expression::Number(1.0)), Box::new(Expression::Number(2.0))))]
+    #[case::expr_1_times_2_times_3("1 * 2 * 3",
+        Expression::Multiply(
+            Box::new(Expression::Multiply(
+                Box::new(Expression::Number(1.0)),
+                Box::new(Expression::Number(2.0))
+            )),
+            Box::new(Expression::Number(3.0))
+        )
+    )]
+    fn parse_to_ast_multiplication(#[case] expression: &str, #[case] expected_ast: Expression) {
+        let mut parser = Parser::new(expression).unwrap();
+
+        let ast = parser.parse().unwrap();
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[rstest]
+    fn parse_to_ast_division() {
+        let expression = "1 / 2";
+        let mut parser = Parser::new(expression).unwrap();
+
+        let ast = parser.parse().unwrap();
+        assert_eq!(ast, Expression::Divide(Box::new(Expression::Number(1.0)), Box::new(Expression::Number(2.0))));
+    }
+
+    #[rstest]
+    #[case::expr_1_plus_2("1 + 2", Expression::Add(Box::new(Expression::Number(1.0)), Box::new(Expression::Number(2.0))))]
+    fn parse_to_ast_addition(#[case] expression: &str, #[case] expected_ast: Expression) {
+        let mut parser = Parser::new(expression).unwrap();
+
+        let ast = parser.parse().unwrap();
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[rstest]
+    #[case::expr_1_minus_2("1 - 2", Expression::Subtract(Box::new(Expression::Number(1.0)), Box::new(Expression::Number(2.0))))]
+    fn parse_to_ast_subtraction(#[case] expression: &str, #[case] expected_ast: Expression) {
+        let mut parser = Parser::new(expression).unwrap();
+
+        let ast = parser.parse().unwrap();
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[rstest]
+    #[case::expr_1_plus_2_minus_3("1 + 2 - 3",
+        Expression::Subtract(
+            Box::new(Expression::Add(
+                Box::new(Expression::Number(1.0)),
+                Box::new(Expression::Number(2.0))
+            )),
+            Box::new(Expression::Number(3.0))
+        )
+    )]
+    #[case::expr_2_times_4_plus_6_div_2("2 * 4 + 6 / 2",
+        Expression::Add(
+            Box::new(Expression::Multiply(
+                Box::new(Expression::Number(2.0)),
+                Box::new(Expression::Number(4.0))
+            )),
+            Box::new(Expression::Divide(
+                Box::new(Expression::Number(6.0)),
+                Box::new(Expression::Number(2.0))
+            ))
+        )
+    )]
+    fn parse_to_ast_complex_expression(#[case] expression: &str, #[case] expected_ast: Expression) {
+        let mut parser = Parser::new(expression).unwrap();
+
+        let ast = parser.parse().unwrap();
+        assert_eq!(ast, expected_ast);
+    }
+
+
+}
