f391c2259aa8fd0138371157df2bafeeaa91011d
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-11-23 22:06:08 +0100

    Update notebook#1 - add function

--- code/hello-world/src/main.rs
+++ code/hello-world/src/main.rs
@@ -4,11 +4,11 @@ const GREETING: &'static str = "Hello, ";
 
 fn input(prompt: &str) -> String {
     static mut COUNTER: u32 = 0;
-    
+
     unsafe {
         COUNTER += 1;
         println!("Function called {} times", COUNTER);
-    }   
+    }
 
     print!("{}: ", prompt);
     std::io::stdout().flush().unwrap(); // Ensure the prompt is printed before reading input
@@ -23,7 +23,7 @@ fn main() {
     println!("{}", full_greeting);
 
     let age_str = input("Get your age: ");
-    
+
     match age_str.parse::<u32>() {
         Ok(age) => println!("You are {} years old.", age),
         Err(_) => println!("Invalid age input."),
