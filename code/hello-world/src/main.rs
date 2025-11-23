55e65fa71731a6023d829544b1cd13b6347f6435
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-11-23 17:29:04 +0100

    hello-world - refactor main function to include user input for name and age with greeting output

--- code/hello-world/src/main.rs
+++ code/hello-world/src/main.rs
@@ -1,3 +1,31 @@
+use std::io::Write;
+
+const GREETING: &'static str = "Hello, ";
+
+fn input(prompt: &str) -> String {
+    static mut COUNTER: u32 = 0;
+    
+    unsafe {
+        COUNTER += 1;
+        println!("Function called {} times", COUNTER);
+    }   
+
+    print!("{}: ", prompt);
+    std::io::stdout().flush().unwrap(); // Ensure the prompt is printed before reading input
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+    name.trim().to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let name = input("Get your name: ");
+    let full_greeting = GREETING.to_string() + name.trim() + "!";
+    println!("{}", full_greeting);
+
+    let age_str = input("Get your age: ");
+    
+    match age_str.parse::<u32>() {
+        Ok(age) => println!("You are {} years old.", age),
+        Err(_) => println!("Invalid age input."),
+    }
 }
